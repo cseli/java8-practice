b80ea42288e6d6d4d360c5235cfd4962c765e45b
Peter Cselenko <Peter.Cselenko@logmein.com>
2017-10-29 18:50:59 +0100

    checkedQueue practice

---
+++ src/main/java/com/bold360/collections/QueueImprovement.java
@@ -0,0 +1,34 @@
+package com.bold360.collections;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+public class QueueImprovement {
+	private Queue<String> queue;
+	private Queue<String> queue8;
+
+	public QueueImprovement() {
+		this.queue = new LinkedList<>();
+		this.queue8 = Collections.checkedQueue(new LinkedList<>(), String.class);
+	}
+
+	Queue<String> getQueue() {
+		return queue;
+	}
+
+	Queue<String> getQueue8() {
+		return queue8;
+	}
+
+	String work() {
+		return queue.stream().collect(Collectors.joining());
+	}
+
+	String work8() {
+		return queue8.stream().collect(Collectors.joining());
+	}
+
+}
+
---
+++ src/test/java/com/bold360/collections/QueueImprovementTest.java
@@ -0,0 +1,39 @@
+package com.bold360.collections;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Queue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueueImprovementTest {
+	QueueImprovement queueImprovement;
+	Queue testQueue;
+
+	@Before
+	public void setUp() throws Exception {
+		queueImprovement = new QueueImprovement();
+	}
+
+	@Test
+	public void testInsertQueue() throws Exception {
+		testQueue = queueImprovement.getQueue();
+		testQueue.add(1);
+		testQueue.add("Hello");
+		assertTrue("Unfortunately it is allowed because we forget the type parameter", testQueue.size() == 2);
+	}
+
+	@Test(expected = ClassCastException.class)
+	public void testInsertAndExecuteQueue() throws Exception {
+		testQueue = queueImprovement.getQueue();
+		testQueue.add(1);
+		testQueue.add("Hello");
+		queueImprovement.work();
+	}
+
+	@Test(expected = ClassCastException.class)
+	public void testInsertQueue8() throws Exception {
+		testQueue = queueImprovement.getQueue8();
+		testQueue.add(1);
+	}
+}
